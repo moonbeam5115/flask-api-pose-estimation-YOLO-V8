793ad755522cfda5b9743b46f8c3b3e94b9f3204
moonbeam5115 <fabritecht@gmail.com>
2024-01-27 15:49:02 -0500

    update

--- pose_api/blueprints/image_pose_estimation/image_pose_estimation.py
+++ pose_api/blueprints/image_pose_estimation/image_pose_estimation.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 from pose_api.tools.extract_kps_image import kps_extractor
 import base64
+import requests
 
 bp = Blueprint('image_pose_estimation', __name__)
 
@@ -16,15 +17,20 @@ def estimate_joint_angles_image():
     try:
         kp_data = [] 
         if request.method == 'POST':
-            image_filestr = request.files['image'].read()
-            with open("imageToSave.png", "wb") as f:
-                f.write(base64.decodebytes(image_filestr))
+            # image_filestr = request.files['image'].read()
+            # with open("imageToSave.png", "wb") as f:
+            #     f.write(base64.decodebytes(image_filestr))
 
             # image_bytes = np.fromstring(image_filestr, np.uint8)
             # img = cv2.imdecode(image_bytes, cv2.IMREAD_UNCHANGED)
-
-                kp_data = kps_extractor(image=f)
-                print(kp_data)
+            image_url = request.image
+            img_data = requests.get(image_url).content
+            img_name = 'image_from_url.jpg'
+            with open(f'{img_name}', 'wb') as handler:
+                handler.write(img_data)
+            
+            kp_data = kps_extractor(image=f'./{img_name}')
+            print(kp_data)
 
             return kp_data
     except Exception as e:
